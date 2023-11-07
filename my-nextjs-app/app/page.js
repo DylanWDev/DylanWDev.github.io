1b47cb61797539ae73ad15058406a377fab41854
Dylan Wyrick <xpoweroutage@gmail.com>
2023-11-07 17:38:17 -0500

    almost got my colors

--- my-nextjs-app/app/page.js
+++ my-nextjs-app/app/page.js
@@ -3,13 +3,13 @@ import React from "react";
 import "bootstrap/dist/css/bootstrap.css";
 import "bootstrap/dist/js/bootstrap.min.js";
 
-export default function Home() { // Capitalize the component name (Home instead of home)
+export default function Home() {
     return (
         <div id="home_page">
-            <div className="d-flex justify-content-start align-items-center" style={{ paddingTop: "30vh", paddingLeft: "5%" }}> {/* Use className instead of class, and use an object for inline styles */}
-                <div id="about_text">
-                    <h1 id="intro">Hi, My <br/>name is <span style={{ color: "#2E5E4E", fontWeight: "bold" }}>Dylan</span> </h1> {/* Use an object for inline styles */}
-                    <h5 id="about">I'm a <span style={{ fontWeight: "bold", color: "#2E5E4E" }}>software developer</span> from Lexington, KY</h5> {/* Use an object for inline styles */}
+            <div id="home_page" className="d-flex justify-content-center align-items-center" style={{ height: "100vh" }}>
+                <div className="text-center">
+                    <h1 id="intro">Hello, I'm <span id="intro_color">Dylan Wyrick</span></h1>
+                    <h1 id="intro">I'm a <span id="intro_color">full stack</span> web developer.</h1>
                 </div>
             </div>
         </div>
