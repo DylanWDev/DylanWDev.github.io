51c410a044c989f979d799a3519414eea0ac934f
w3schools-test <test@w3schools.com>
2023-11-07 20:55:32 -0500

    added stars to home page

--- my-nextjs-app/app/page.js
+++ my-nextjs-app/app/page.js
@@ -5,13 +5,16 @@ import "bootstrap/dist/js/bootstrap.min.js";
 
 export default function Home() {
     return (
-        <div id="home_page">
-            <div id="home_page" className="d-flex justify-content-center align-items-center" style={{ height: "100vh" }}>
-                <div className="text-center">
-                    <h1 id="intro">Hello, I'm <span id="intro_color">Dylan Wyrick</span></h1>
-                    <h1 id="intro">I'm a <span id="intro_color">full stack</span> web developer.</h1>
-                </div>
-            </div>
+        <div>
+            <section>
+                <div id="stars"></div>
+                <div id="stars2"></div>
+                <div id="stars3"></div>
+                <div id="title"></div>
+                    <span>Hello, I'm Dylan Wyrick</span>
+                    <br/>
+                    <span>I'm a full stack web developer</span>
+            </section>
         </div>
     );
 }
